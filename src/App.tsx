bfe089a7fc41f0fab060a5b48031c50b136c4ba6
ahmedswar <ahmedswar68@gmail.com>
2023-12-03 06:40:49 +0200

    dynamic products list

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
 import ProductCard from "./components/ProductCard";
+import { productList } from "./data";
 
 function App() {
+  const productsRenderList = productList.map((product)=><ProductCard product={product} key={product.id}/>)
+
   return (
     <>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 ">
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
+        {productsRenderList}
       </div>
     </>
   );
--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,29 +1,25 @@
+import { IProduct } from "../interfaces";
 import Image from "./Image";
 import Button from "./ui/Button";
 
-interface IProps {}
+interface IProps {
+  product: IProduct;
+}
 
-const ProductCard = ({}: IProps) => {
+const ProductCard = ({ product }: IProps) => {
+  const { title, imageURL, description, price } = product;
   return (
     <div className="border rounded-md p-2 flex flex-col">
-      <Image
-        alt="productName"
-        className="rounded-md"
-        imageUrl="https://efreshli-uploads.s3.eu-central-1.amazonaws.com/products/Pp9pyS1UI1xAvvxNsVIvgXOYlladOPizULpqjJpP.png"
-      />
-      <h3>Quattro Macrame</h3>
-      <p>
-        Wooven chest offers essential storage for your nursery. Crafted from
-        solid natural beech wood with a warm espresso finish, this 3-drawer unit
-        features soft-closing drawers for quiet organization
-      </p>
+      <Image alt="productName" className="rounded-md" imageUrl={imageURL} />
+      <h3>{title}</h3>
+      <p>{description}</p>
       <div className="flex items-center my-4 space-x-2">
         <span className="w-5 h-5 rounded-full bg-black cursor-pointer" />
         <span className="w-5 h-5 rounded-full bg-yellow-500 cursor-pointer" />
         <span className="w-5 h-5 rounded-full bg-red-900 cursor-pointer" />
       </div>
       <div className="flex items-center justify-between ">
-        <span>$1,000 </span>
+        <span>{price}</span>
         <Image
           alt="productName"
           className="w-10 h-10 rounded-full object-bottom"
