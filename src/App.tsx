3d163a7929d930d37e5270b0cda088c3acf7c385
ahmedswar <ahmedswar68@gmail.com>
2023-12-04 10:22:02 +0200

    fix close modal

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,9 @@ function App() {
       imageURL: "",
     },
   });
+
+  const closeModal = () => setIsOpen(false);
+  const openModal = () => setIsOpen(true);
   const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
     const { value, name } = event.target;
 
@@ -26,43 +29,48 @@ function App() {
       [name]: value,
     });
   };
+
   const productsRenderList = productList.map((product) => (
     <ProductCard product={product} key={product.id} />
   ));
+
   const formRenderList = formInputsList.map((input) => (
     <div className="flex flex-col" key={input.id}>
-      <label htmlFor={input.id} className="mb-[2px] text-sm font-medium text-gray-700">
+      <label
+        htmlFor={input.id}
+        className="mb-[2px] text-sm font-medium text-gray-700"
+      >
         {input.label}
       </label>
-      <Input type="text" id={input.id} name={input.name} value={product[input.name]} onChange={onChangeHandler}/>
+      <Input
+        type="text"
+        id={input.id}
+        name={input.name}
+        value={product[input.name]}
+        onChange={onChangeHandler}
+      />
     </div>
   ));
-  
 
-  function closeModal() {
-    setIsOpen(false);
-  }
-
-  function openModal() {
-    setIsOpen(true);
-  }
   return (
     <div className="container">
-      <Button className="bg-blue-600" onClick={openModal}>
+      <Button
+        onClick={openModal}
+        className="block bg-indigo-700 hover:bg-indigo-800 mx-auto my-10 px-10 font-medium"
+      >
         Add new
       </Button>
-      <Modal
-        isOpen={isOpen}
-        title="Add a new product"
-        closeModal={() => closeModal}
-      >
+      <Modal isOpen={isOpen} title="Add a new product" closeModal={closeModal}>
         <form className="space-y-3">
           {formRenderList}
-        <div className="flex items-center justify-between space-x-3 mt-5">
-          <Button className="bg-blue-600">Edit</Button>
-          <Button className="bg-red-600">Delete</Button>
-        </div>
-        
+          <div className="flex items-center space-x-3">
+            <Button className="bg-indigo-700 hover:bg-indigo-800">
+              Submit
+            </Button>
+            <Button className="bg-[#f5f5fa] hover:bg-gray-300 !text-black">
+              Cancel
+            </Button>
+          </div>
         </form>
       </Modal>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 ">
